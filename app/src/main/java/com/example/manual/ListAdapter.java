b8e0899c63c422eb38e84cdeef28b49a1c5eaea6
jin0220 <jin__0220@naver.com>
2020-08-14 15:29:03 +0900

    1

--- app/src/main/java/com/example/manual/ListAdapter.java
+++ app/src/main/java/com/example/manual/ListAdapter.java
@@ -1,10 +1,12 @@
 package com.example.manual;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -47,13 +49,18 @@ public class ListAdapter extends BaseAdapter {
 
         textView.setText(listData.getTitle());
 
+        ImageView imageView;
+        imageView = view.findViewById(R.id.image);
+        imageView.setImageDrawable(listData.getIcon());
+
         return view;
     }
 
-    public void addData(String title){
+    public void addData(String title, Drawable icon){
         ListData item = new ListData();
 
         item.setTitle(title);
+        item.setIcon(icon);
 
         items.add(item);
     }
